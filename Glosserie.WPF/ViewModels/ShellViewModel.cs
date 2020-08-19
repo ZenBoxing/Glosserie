3445a83de66ef7eec9cd1cad09404be66e6dccd3
Jarrett Lirette <jarrettlirette@gmail.com>
2020-08-19 13:07:55 -0400

    Began Login Page design
    added fonts, styles, and controls for login page

--- Glosserie.WPF/ViewModels/ShellViewModel.cs
+++ Glosserie.WPF/ViewModels/ShellViewModel.cs
@@ -6,6 +6,13 @@ namespace Glosserie.WPF.ViewModels
 {
     public class ShellViewModel : ViewModelBase
     {
+		private ViewModelBase _activeViewModel = new LoginViewModel();
 
-    }
+		public ViewModelBase ActiveViewModel
+		{
+			get { return _activeViewModel; }
+			set { _activeViewModel = value; }
+		}
+
+	}
 }
