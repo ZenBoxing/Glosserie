37e68ba961f1296d12e7d8b9c98b8f18eeb69c4b
Jarrett Lirette <jarrettlirette@gmail.com>
2021-12-14 10:55:33 -0500

    Added dependancy property IsOpen to CreateVocabListForm Added data triggers to MainControlPanel on CreateVocabListForm

--- Glosserie.WPF/Controls/CreateVocabListForm.xaml.cs
+++ Glosserie.WPF/Controls/CreateVocabListForm.xaml.cs
@@ -22,5 +22,19 @@ namespace Glosserie.WPF.Controls
         {
             InitializeComponent();
         }
+
+
+
+        public bool IsOpen
+        {
+            get { return (bool)GetValue(IsOpenProperty); }
+            set { SetValue(IsOpenProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for IsOpen.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty IsOpenProperty =
+            DependencyProperty.Register("IsOpen", typeof(bool), typeof(CreateVocabListForm), new PropertyMetadata(false));
+
+
     }
 }
