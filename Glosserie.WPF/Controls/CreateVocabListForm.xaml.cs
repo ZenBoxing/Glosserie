e36d21a2c34c4e68229d312f03a2b1aa2f260741
Jarrett Lirette <jarrettlirette@gmail.com>
2021-12-20 12:55:08 -0500

    Added RoutedEvent OpenDropDownEvent to CreateVocabListForm

--- Glosserie.WPF/Controls/CreateVocabListForm.xaml.cs
+++ Glosserie.WPF/Controls/CreateVocabListForm.xaml.cs
@@ -28,13 +28,20 @@ namespace Glosserie.WPF.Controls
         public bool IsOpen
         {
             get { return (bool)GetValue(IsOpenProperty); }
-            set { SetValue(IsOpenProperty, value); }
+            set { SetValue(IsOpenProperty, value);}
         }
 
         // Using a DependencyProperty as the backing store for IsOpen.  This enables animation, styling, binding, etc...
         public static readonly DependencyProperty IsOpenProperty =
             DependencyProperty.Register("IsOpen", typeof(bool), typeof(CreateVocabListForm), new PropertyMetadata(false));
 
+        public static readonly RoutedEvent OpenDropDownEvent = EventManager.RegisterRoutedEvent("OpenDropDown",RoutingStrategy.Bubble,
+            typeof(RoutedEventHandler), typeof(CreateVocabListForm));
 
+        public event RoutedEventHandler OpenDropDown
+        {
+            add { AddHandler(OpenDropDownEvent, value); }
+            remove { RemoveHandler(OpenDropDownEvent, value); }
+        }
     }
 }
