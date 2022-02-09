c8ffa9673fe3b266a10f385a721d357a3d7a771e
Jarrett Lirette <jarrettlirette@gmail.com>
2022-02-09 12:02:02 -0500

    Added OpenFileDialog box to CreateVocabListForm

--- Glosserie.WPF/ViewModels/HomeViewModel.cs
+++ Glosserie.WPF/ViewModels/HomeViewModel.cs
@@ -1,10 +1,12 @@
 ﻿using Glosserie.WPF.Commands;
 using Glosserie.WPF.Library.Models;
 using Glosserie.WPF.Library.Services;
+using Microsoft.Win32;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Text;
+using System.IO;
 using System.Threading.Tasks;
 using System.Windows.Input;
 
@@ -13,6 +15,8 @@ namespace Glosserie.WPF.ViewModels
     public class HomeViewModel : ViewModelBase
     {
         public VocabListsViewModel VocabListsViewModel { get; set; }
+
+        public VocabListOptionsModel VocabListOptionsModel { get; set; }
 
         private bool _isCreateListModalOpen;
         public bool IsCreateListModalOpen
@@ -30,14 +34,27 @@ namespace Glosserie.WPF.ViewModels
 
         public ICommand OpenCreateListModalCommand { get; }
         public ICommand CloseCreateListModalCommand { get; }
+        public ICommand OpenFileDialogBoxCommand { get; }
 
         public HomeViewModel(VocabListsViewModel vocabListsViewModel)
         {
             VocabListsViewModel = vocabListsViewModel;
+            VocabListOptionsModel = new VocabListOptionsModel();
 
             OpenCreateListModalCommand = new DelegateCommand(() => IsCreateListModalOpen = true);
             CloseCreateListModalCommand = new DelegateCommand(() => IsCreateListModalOpen = false);
+            OpenFileDialogBoxCommand = new DelegateCommand(OpenFileDialogBox);
         }
 
+        public void OpenFileDialogBox()
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "PDF Files (*.pdf)| *.pdf";
+            if(openFileDialog.ShowDialog() == true)
+            {
+                VocabListOptionsModel.FileContents = File.ReadAllBytes(openFileDialog.FileName);
+            }
+            //openFileDialog.ShowDialog();
+        }
 	}
 }
